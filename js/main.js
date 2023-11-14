2aaee9565e548bc81887796bfbca7c9f3beec0c3
2joopiter <veryberrylike3@gmail.com>
2023-11-14 13:15:35 +0900

    현재 시간 출력 만들기

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-const h1 = document.querySelector('h1');
-const [btnStart, btnStop] = document.querySelectorAll('button');
-//전역변수 : 우주에 떠있는 인공위성(중계기)
+const [spanHr, spanMin, spanSec] = document.querySelectorAll('span');
 
-let num = 0;
-let timer = null;
+const hr = new Date().getHours();
+const min = new Date().getMinutes();
+const sec = new Date().getSeconds();
 
-timer = setInterval(() => {
-	//지역변수: A 지역에서 변경요청
-	h1.innerText = num++;
-}, 1000);
+spanHr.innerText = hr;
+spanMin.innerText = min;
+spanSec.innerText = sec;
 
-btnStop.addEventListener('click', () => {
-	clearInterval(timer);
-});
-
-btnStart.addEventListener('click', () => {
-	timer = setInterval(() => {
-		//B 지역에서 변경요청
-		h1.innerText = num++;
-	}, 1000);
-});

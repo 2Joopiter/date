083af18b409053407c788edf169c496ad8a9d6a5
2joopiter <veryberrylike3@gmail.com>
2023-11-14 13:08:58 +0900

    전역변수의 비유

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const h1 = document.querySelector('h1');
 const [btnStart, btnStop] = document.querySelectorAll('button');
+//전역변수 : 우주에 떠있는 인공위성(중계기)
 
 let num = 0;
 let timer = null;
 
 timer = setInterval(() => {
+	//지역변수: A 지역에서 변경요청
 	h1.innerText = num++;
 }, 1000);
 
@@ -14,6 +16,7 @@ btnStop.addEventListener('click', () => {
 
 btnStart.addEventListener('click', () => {
 	timer = setInterval(() => {
+		//B 지역에서 변경요청
 		h1.innerText = num++;
 	}, 1000);
 });
